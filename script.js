44f509b2c0ed5c9648cdeb6974f0f988ce6782a3
SaraToth <saratoth101@gmail.com>
2025-01-13 18:16:13 +0900

    Create an empty 4x4 graph

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+//Number of vertices - which determines grid rows/columns
+const V = 4;
+
+//Creates an empty V x V graph
+let graph = Array.from({length: V}, ()=> Array(V).fill(0));
+
+console.log(graph);
