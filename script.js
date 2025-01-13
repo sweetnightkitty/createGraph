ae14d94023f138da74eca2347dfa9ad817efe209
SaraToth <saratoth101@gmail.com>
2025-01-13 18:18:35 +0900

    Write function addEdge that takes two vertices and creates an edge in the graph

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ const V = 4;
 //Creates an empty V x V graph
 let graph = Array.from({length: V}, ()=> Array(V).fill(0));
 
-console.log(graph);
+//Creates an edge between two vertices a and b
+function addEdge(graph, a, b) {
+    graph[a][b] = 1;
+    graph[b][a] = 1; // an undirected graph
+}
+
